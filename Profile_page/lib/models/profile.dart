544d427862412dea7526851a2119be190529e793
paulamlago <pmunoz06@ucm.es>
2021-03-05 17:48:35 +0100

    removed profile page

--- Profile_page/lib/models/profile.dart
+++
@@ -1,9 +0,0 @@
-class Profile {
-  final String imageUrl;
-  final String name;
-  final List<String> phoneNumbers;
-  final String email;
-  final List<String> location;
-
-  Profile({this.imageUrl, this.name, this.phoneNumbers, this.email, this.location});
-}
